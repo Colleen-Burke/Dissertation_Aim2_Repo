93bbee2c452cdbbce165f908c4ebb37c611be5a4
Colleen-Burke <colleen.a.burke@duke.edu>
2026-05-26 12:33:15 -0400

    Renamed

---
+++ xx_04_Adjusted_Old.R
@@ -0,0 +1,132 @@
+library(tidyverse)
+library(here)
+
+
+analytic_data <- analytic_data  |> 
+  mutate(
+    race_f = factor(race_f,
+                    levels = c("American Indian or Alaskan Native", "Asian",
+                               "Native Hawaiian or Other Pacific Islander",
+                               "Black or African American", "White",
+                               "Middle Eastern or North African", "Other",
+                               "Unknown", "Multiracial", "Choose Not to Respond")),
+    ethnicity_f = factor(ethnicity_f,
+                         levels = c("Non-Hispanic", "Hispanic or Latinx"))
+  )
+
+##--- Social Health Dichotomized Model (Adjusted) ---
+social_health_d_model_adj <- glm(
+  chronic_pain ~ social_health_d + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_health_d_model_adj)
+
+##--- Social Health Dichotomized 2 Model (Adjusted) ---
+social_health2_d_model_adj <- glm(
+  chronic_pain ~ social_health2_d + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_health2_d_model_adj)
+
+##--- Social Health Model (Adjusted) ---
+social_health_num_model_adj <- glm(
+  chronic_pain ~ social_health_num + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_health_num_model_adj)
+
+##--- Social Function Model (Adjusted) ---
+social_function_num_model_adj <- glm(
+  chronic_pain ~ social_function_num + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_function_num_model_adj)
+
+##--- Social Activities Model (Adjusted) ---
+social_activities_num_model_adj <- glm(
+  chronic_pain ~ social_activities_num + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_activities_num_model_adj)
+
+##--- Social Relationships Model (Adjusted) ---
+social_relationships_num_model_adj <- glm(
+  chronic_pain ~ social_relationships_num + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_relationships_num_model_adj)
+
+##--- Social Isolation Model (Adjusted) ---
+isolation_score_model_adj <- glm(
+  chronic_pain ~ isolation_score + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(isolation_score_model_adj)
+
+##--- Social Activities Dichotomized Model (Adjusted) ---
+social_activities_d_model_adj <- glm(
+  chronic_pain ~ social_activities_d + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_activities_d_model_adj)
+
+##--- Social Relationships Dichotomized Model (Adjusted) ---
+social_relationships_d_model_adj <- glm(
+  chronic_pain ~ social_relationships_d + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_relationships_d_model_adj)
+
+##--- Social Isolation Dichotomized Model (Adjusted) ---
+isolation_cat_d_model_adj <- glm(
+  chronic_pain ~ isolation_cat_d + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(isolation_cat_d_model_adj)
+
+##--- Social Activities Dichotomized 2 Model (Adjusted) ---
+social_activities2_d_model_adj <- glm(
+  chronic_pain ~ social_activities2_d + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_activities2_d_model_adj)
+
+##--- Social Relationships Dichotomized 2 Model (Adjusted) ---
+social_relationships2_d_model_adj <- glm(
+  chronic_pain ~ social_relationships2_d + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(social_relationships2_d_model_adj)
+
+##--- Social Isolation Dichotomized 2 Model (Adjusted) ---
+isolation_cat2_d_model_adj <- glm(
+  chronic_pain ~ isolation_cat2_d + 
+    age + sex + race_f + ethnicity_f + bmi_measured + general_health + 
+    mental_health + psqi_global_score + lbp_vas_current,
+  data = analytic_data,
+  family = binomial)
+summary(isolation_cat2_d_model_adj)
